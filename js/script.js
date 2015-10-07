595510bc59b9fe5a9f17f64bca1ef45cafc0c26e
xeqtr87 <alidaman6@hotmail.com>
2015-10-07 11:58:56 +0000

    added jquery form

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-function validate()
+$(document).ready(function() {
+$('#contact_name').on('input', function() {
+	var input=$(this);
+	var is_name=input.val();
+	if(is_name){input.removeClass("invalid").addClass("valid");}
+	else{input.removeClass("valid").addClass("invalid");}
+});
+});
