53a0796651bd1890fbc149fb913d3c189978c232
Linus <juhlinus@gmail.com>
2015-10-07 19:22:48 +0200

    Yay, progress!

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-$('#contact_name').on('input', function() {
-	var input=$(this);
-	var is_name=input.val();
-	if(is_name){input.removeClass("invalid").addClass("valid");}
-	else{input.removeClass("valid").addClass("invalid");}
-});
+	$('#contact_name').on('input', function() {
+		var input=$(this);
+		var is_name=input.val();
+		console.log(is_name);
+		if(is_name){input.removeClass("invalid").addClass("valid");}
+		else{input.removeClass("valid").addClass("invalid");}
+	});
 });
