454a619f42ef3f03c7e9a57befa29c1c5cfe349b
xeqtr87 <alidaman6@hotmail.com>
2015-10-09 09:53:04 +0000

    small fixes script/kontakt

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 	$('#contact_name').on('input', function() {
 		var input=$(this);
-		var is_name=input.val();
-		console.log(is_name);
-		if(is_name){input.removeClass("invalid").addClass("valid");}
+		var nameForm=input.val();
+		console.log(nameForm);
+		if(nameForm){input.removeClass("invalid").addClass("valid");}
 		else{input.removeClass("valid").addClass("invalid");}
 	});
 });
