7e60e8eeb66421e3b5d081b9098aa11d07f204af
xeqtr87 <alidaman6@hotmail.com>
2015-10-11 17:07:00 +0000

    added email & question

--- js/script.js
+++ js/script.js
@@ -13,9 +13,24 @@ $(document).ready(function()
 			var input=$(this);
 			var telForm=input.val();
 			console.log(telForm);
-			//if(telForm){input.removeClass("invalid").addClass("valid");}
-		//	else{input.removeClass("valid").addClass("invalid");}
-		
+			if(telForm){input.removeClass("invalid").addClass("valid");}
+			else{input.removeClass("valid").addClass("invalid");}
+		});
+		$("#contact_email").on("input", function()
+		{
+			var input=$(this);
+			var emailForm=input.val();
+			console.log(emailForm);
+			if(emailForm){input.removeClass("invalid").addClass("valid");}
+			else{input.removeClass("valid").addClass("invalid");}
+		});
+		$("#contact_que").on("input",function()
+		{
+			var input=$(this);
+			var queForm=input.val();
+			console.log(queForm);
+			if(queForm){input.addClass("valid").removeClass("invalid");}
+			else{input.addClass("invalid").removeClass("valid");}
 		});
 });
 
