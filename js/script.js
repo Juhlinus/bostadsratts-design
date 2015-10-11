b70f2a06098f0052fb3c8a1037d5d97f32fe4850
xeqtr87 <alidaman6@hotmail.com>
2015-10-11 16:54:02 +0000

    added telefonnummer

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
-$(document).ready(function() {
-	$('#contact_name').on('input', function() {
-		var input=$(this);
-		var nameForm=input.val();
-		console.log(nameForm);
-		if(nameForm){input.removeClass("invalid").addClass("valid");}
-		else{input.removeClass("valid").addClass("invalid");}
-	});
+$(document).ready(function()
+	{
+		$("#contact_name").on("input", function()
+		{
+			var input=$(this);
+			var nameForm=input.val(); //input value
+			console.log(nameForm);
+			if(nameForm){input.removeClass("invalid").addClass("valid");}
+			else{input.removeClass("valid").addClass("invalid");}
+		});
+		$("#contact_tel").on("input", function()
+		{
+			var input=$(this);
+			var telForm=input.val();
+			console.log(telForm);
+			//if(telForm){input.removeClass("invalid").addClass("valid");}
+		//	else{input.removeClass("valid").addClass("invalid");}
+		
+		});
 });
+
+	
