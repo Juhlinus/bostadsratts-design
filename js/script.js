c99bea36142f464eaf547fb88b43267405c52728
xeqtr87 <alidaman6@hotmail.com>
2015-10-12 12:00:46 +0000

    added comments

--- js/script.js
+++ js/script.js
@@ -1,57 +1,61 @@
-$(document).ready(function()
-	{
-		var inputName;
-		var inputTel;
-		var inputQue;
-		var inputEmail;
+$(document).ready(function() {
+	var inputName;
+	var inputTel;
+	var inputQue;
+	var inputEmail;
+
+	$("#contact_name").on("input", function() {
+		inputName = $(this);
+		var nameForm = inputName.val();
+		validate(inputName, nameForm);
+	});
+	$("#contact_tel").on("input", function() {
+		inputTel = $(this);
+		var telForm = inputTel.val();
+		validate(inputTel, telForm);
+	});
+	$("#contact_email").on("input", function() {
+		inputEmail = $(this);
+		var inputText = inputEmail.val();
+		validate(inputEmail, inputText);
+	});
+	$("#contact_que").on("input", function() {
+		inputQue = $(this);
+		var queForm = inputQue.val();
+		console.log(queForm);
+		validate(inputQue, queForm);
+	});
+	$("form").submit(function(event) { //------------------------- Submit button
 		
-		$("#contact_name").on("input", function()
-		{
-			inputName = $(this);
-			var nameForm=inputName.val(); 
-			console.log(nameForm);
-			if(nameForm){inputName.removeClass("invalid").addClass("valid");}
-			else{inputName.removeClass("valid").addClass("invalid");}
-		});
-		$("#contact_tel").on("input", function()
-		{
-			inputTel=$(this);
-			var telForm=inputTel.val();
-			console.log(telForm);
-			if(telForm){inputTel.removeClass("invalid").addClass("valid");}
-			else{inputTel.removeClass("valid").addClass("invalid");}
-		});
-		$("#contact_email").on("input", function()
-		{
-			inputEmail=$(this);
-			var emailForm=inputEmail.val();
-			console.log(emailForm);
-			if(emailForm){inputEmail.removeClass("invalid").addClass("valid");}
-			else{inputEmail.removeClass("valid").addClass("invalid");}
-		});
-		$("#contact_que").on("input",function()
-		{
-			inputQue=$(this);
-			var queForm=inputQue.val();
-			console.log(queForm);
-			if(queForm){inputQue.addClass("valid").removeClass("invalid");}
-			else{inputQue.addClass("invalid").removeClass("valid");}
-		});
-		$("form").submit(function(event)
-		{	console.log(inputEmail);
-		console.log(inputEmail.hasClass("invalid"));
-			if(inputName.hasClass("invalid") || inputTel.hasClass("invalid") || inputQue.hasClass("invalid") || inputEmail.hasClass("invalid"))
-			{
-				event.preventDefault();
-				console.log("hejsan");
-			}
-			else
-			{
-				console.log("Vi kontaktar dig inom 3 arbetsdagar.");
-			}
-			
-		});
+		
+		if (inputName === undefined) //------------------------- If the inputName is undefined 
+			inputName = $("#contact_name"); //------------------------- Assign inputName the object
+		if (inputTel === undefined)
+			inputTel = $("#contact_tel");
+		if (inputEmail === undefined)
+			inputEmail = $("#contact_email");
+		if (inputQue === undefined)
+			inputQue = $("#contact_que");
 
-			
-});	
+		validate(inputTel, inputTel.val()); //------------------------- Validate inputTel, line 51
+		if (inputName.hasClass("invalid") || inputTel.hasClass("invalid") || inputQue.hasClass("invalid") || inputEmail.hasClass("invalid")) {
+			event.preventDefault();
+			console.log("hejsan");
+		}
+		else {
+			console.log("Vi kontaktar dig inom 3 arbetsdagar.");
+		}
 
+	});
+
+function validate(input, text) { 
+	if (text) { //------------------------- if the user typed anything
+		input.removeClass("invalid").addClass("valid");
+
+	}
+	else {
+		input.removeClass("valid").addClass("invalid");
+
+	}
+}
+});
